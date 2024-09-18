d8349e3cce18375c505b04009fcf584b330156a3
baunilha <heitor326@hotmail.com>
2024-09-18 19:57:28 -0300

    🚀 novas métricas

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
-import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
 import numpy as np
-from keras import backend
-from sklearn.metrics.classification import accuracy_score
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+import os
 import argparse
-import sys
 
-sys.path.insert(0, '../utils')
-import custom_functions as func
-import custom_callbacks
+# Load CIFAR-10 dataset
+def load_cifar10_data():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    
+    # Normalize the data to [0, 1] range
+    X_train = X_train.astype('float32') / 255.0
+    X_test = X_test.astype('float32') / 255.0
+    
+    # One-hot encode the labels
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
+    
+    return X_train, y_train, X_test, y_test
+
+# Learning rate scheduler
+def lr_scheduler(epoch, lr):
+    if epoch < 25:
+        return lr
+    elif epoch < 75:
+        return 1e-3
+    else:
+        return 1e-4
 
 if __name__ == '__main__':
     np.random.seed(12227)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--architecture', type=str, default='../../architectures/CIFAR10/ResNet20')
-    parser.add_argument('--weights', type=str, default='../../weights/CIFAR10/ResNet20++')
+    parser.add_argument('--architecture', type=str, default='ResNet50')
+    parser.add_argument('--dataset', type=str, default='CIFAR10')
+    parser.add_argument('--weights', type=str, default='')
     parser.add_argument('--model_name', type=str, default='')
 
     args = parser.parse_args()
     architecture = args.architecture
-    weights = args.weights
+    dataset_name = args.dataset
+    weights = args.weights if args.weights != '' else f'./weights/{dataset_name}/{architecture}'
     model_name = args.model_name
 
     model_name = architecture.split('/')[-1] if model_name == '' else model_name
     print(model_name, flush=True)
 
-    model = func.load_model(architecture,
-                            weights)
+    # Create directory for saving weights if it doesn't exist
+    os.makedirs(weights, exist_ok=True)
 
-    X_train, y_train, X_test, y_test = func.cifar_resnet_data(debug=False)
+    # Load the CIFAR-10 dataset
+    X_train, y_train, X_test, y_test = load_cifar10_data()
 
-    # Configura o Data Augmentation.
+    # Load a pre-trained ResNet model (without the top layer, and with input shape for CIFAR-10)
+    model = ResNet50(weights=None, input_shape=(32, 32, 3), classes=10)
+
+    # Configure the optimizer
+    lr = 0.01
+    optimizer = SGD(learning_rate=lr, momentum=0.9)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Configure the Data Augmentation
     datagen = ImageDataGenerator(
         featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
@@ -43,29 +74,33 @@ if __name__ == '__main__':
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-    # preprocessing_function=func.cutout)
+        vertical_flip=False,  # randomly flip images
+    )
     datagen.fit(X_train)
 
-    lr = 0.01
-    schedule = [(25, 1e-3), (75, 1e-4)]
+    # Set up learning rate scheduler
+    lr_scheduler_callback = tf.keras.callbacks.LearningRateScheduler(lr_scheduler)
 
-    lr_scheduler = custom_callbacks.LearningRateScheduler(init_lr=lr, schedule=schedule)
-    callbacks = [lr_scheduler]
+    # Set up the ModelCheckpoint callback to save the model every n epochs
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(weights, f"{model_name}_epoch_{{epoch:02d}}.weights.h5"),
+        save_weights_only=True,  # Save only the weights
+        save_freq='epoch',  # Save at the end of every epoch
+        verbose=1
+    )
 
-    model = func.optimizer_compile(model, 'VGG16')
+    epochs = 10
+    # Train the model
+    model.fit(datagen.flow(X_train, y_train, batch_size=128),
+              steps_per_epoch=len(X_train) // 128,
+              epochs=epochs,
+              validation_data=(X_test, y_test),
+              callbacks=[lr_scheduler_callback, checkpoint_callback])
 
-    for epoch in range(1, 100):
+    # Save the model weights after training
+    model.save_weights(os.path.join(weights, f'{model_name}_{dataset_name}_{epochs}.weights.h5'))
 
-        model.fit_generator(datagen.flow(X_train, y_train),
-                            steps_per_epoch=5000,
-                            epochs=epoch, initial_epoch=epoch - 1,
-                            verbose=2, callbacks=callbacks)
-        if epoch % 20 == 0:
-            model.save_weights('{}_epoch{}.h5'.format(model_name, epoch))
-
-        if epoch % 5 == 0:
-            acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(model.predict(X_test), axis=1))
-            print('Accuracy [{:.4f}]'.format(acc), flush=True)
-
-    model.save_weights('{}.h5'.format(model_name))
+    # Evaluate model
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+    print(f'Final accuracy: {accuracy:.4f}')
