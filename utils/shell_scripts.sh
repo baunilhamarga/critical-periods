6b3f122a5368a6651ab5322d1d42719e053179b4
baunilhamarga <heitor326@hotmail.com>
2024-09-24 12:26:39 -0300

    ✨ log time

--- utils/shell_scripts.sh
+++ utils/shell_scripts.sh
@@ -3,8 +3,8 @@
 # Define the log_and_run function
 log_and_run() {
     echo -e "\n\n********************************************************************************************************************\n" >> log.log
-    echo "$ nohup python main.py >> log.log 2>&1 &\n" >> log.log
-    nohup python main.py >> log.log 2>&1 &
+    echo "$ nohup /usr/bin/time -v python main.py >> log.log 2>&1 &" >> log.log
+    nohup /usr/bin/time -v python main.py >> log.log 2>&1 &
 }
 
 log_and_run
