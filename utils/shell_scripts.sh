d7baf9314757add81046d9b7291739c3bbb20fe3
baunilhamarga <heitor326@hotmail.com>
2024-10-28 15:50:41 -0300

    ✨ args compatibility

--- utils/shell_scripts.sh
+++ utils/shell_scripts.sh
@@ -3,12 +3,13 @@
 # Define the log_and_run function with an argument for the script name
 log_and_run() {
     local script_name=$1
+    shift
     echo -e "\n\n********************************************************************************************************************\n" >> log.log
     echo "$ nohup /usr/bin/time -v python $script_name >> log.log 2>&1 &" >> log.log
     nohup /usr/bin/time -v python $script_name >> log.log 2>&1 &
 }
 
-log_and_run $1
+log_and_run "$@"
 
 # To stop run
 # $ ps aux | grep 'python main.py'
