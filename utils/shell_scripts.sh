5edc99e79a71b1136934cb633c81490815b2099d
baunilhamarga <heitor326@hotmail.com>
2024-10-29 10:22:12 -0300

    ⚡️ args parsing support and unique logfile names

--- utils/shell_scripts.sh
+++ utils/shell_scripts.sh
@@ -3,10 +3,11 @@
 # Define the log_and_run function with an argument for the script name
 log_and_run() {
     local script_name=$1
-    shift
-    echo -e "\n\n********************************************************************************************************************\n" >> log.log
-    echo "$ nohup /usr/bin/time -v python $script_name >> log.log 2>&1 &" >> log.log
-    nohup /usr/bin/time -v python $script_name >> log.log 2>&1 &
+    local log_file="${script_name%.*}.log"  # Use the script name without the extension as the log file name
+    shift  # Shift to remove the script name from the list of arguments
+    echo -e "\n\n********************************************************************************************************************\n" >> "$log_file"
+    echo "$ nohup /usr/bin/time -v python $script_name $@ >> $log_file 2>&1 &" >> "$log_file"
+    nohup /usr/bin/time -v python "$script_name" "$@" >> "$log_file" 2>&1 &
 }
 
 log_and_run "$@"
