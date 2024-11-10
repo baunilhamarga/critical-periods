0971ef3a8272ba96e865f23b9c48123623e0a007
baunilhamarga <heitor326@hotmail.com>
2024-11-10 10:22:34 -0300

    🚀 paralel jobs log support

--- utils/shell_scripts.sh
+++ utils/shell_scripts.sh
@@ -3,10 +3,27 @@
 # Define the log_and_run function with an argument for the script name
 log_and_run() {
     local script_name=$1
-    local log_file="${script_name%.*}.log"  # Use the script name without the extension as the log file name
-    shift  # Shift to remove the script name from the list of arguments
-    echo -e "\n\n********************************************************************************************************************\n" >> "$log_file"
+    local log_dir="./logs"
+    local base_log_file="${log_dir}/${script_name%.*}"
+    local i=1
+
+    # Create logs directory if it doesn't exist
+    mkdir -p "$log_dir"
+
+    # Find the next available log file name
+    while [[ -e "${base_log_file}_${i}.log" ]]; do
+        ((i++))
+    done
+
+    local log_file="${base_log_file}_${i}.log"
+    shift  # Remove the script name from the list of arguments
+
+    # Print the current date and time at the start of the log file
+    echo "Log started at: $(date)" > "$log_file"
+    echo -e "\n********************************************************************************************************************\n" >> "$log_file"
     echo "$ nohup /usr/bin/time -v python $script_name $@ >> $log_file 2>&1 &" >> "$log_file"
+
+    # Run the command with nohup and append output to the log
     nohup /usr/bin/time -v python "$script_name" "$@" >> "$log_file" 2>&1 &
 }
 
